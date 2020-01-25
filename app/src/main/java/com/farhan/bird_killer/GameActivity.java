4ed5c881172db372af95c6cb5acc3c61b2565e29
Farhan <farhansadikgalib@gmail.com>
2020-01-25 11:34:36 +0600

    finally done this project also debug its a sensual bug..

--- app/src/main/java/com/farhan/bird_killer/GameActivity.java
+++ app/src/main/java/com/farhan/bird_killer/GameActivity.java
@@ -8,31 +8,26 @@ import android.view.WindowManager;
 
 public class GameActivity extends AppCompatActivity {
 
-
     private GameView gameView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         Point point = new Point();
         getWindowManager().getDefaultDisplay().getSize(point);
 
+        gameView = new GameView(this, point.x, point.y);
 
-        gameView = new GameView(this,point.x,point.y);
         setContentView(gameView);
-
     }
-
 
     @Override
     protected void onPause() {
         super.onPause();
         gameView.pause();
-
     }
 
     @Override
